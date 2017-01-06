8d13837c9db16fcdc09a4d0afafd9f63453d58f5
dempseydata <dempseydata@gmail.com>
2017-01-05 16:01:52 -0800

    first file

---
+++ scrapbook.R
@@ -0,0 +1,14 @@
+# various code associated with working theough Hadley's book
+
+# packages instaleld as part of the book
+install.packages('tidyverse')
+
+
+# data packages to be used
+install.packages(c("nycflights13", "gapminder", "Lahman"))
+
+
+# other code
+library('tidyverse')
+
+http://r4ds.had.co.nz/explore-intro.html
